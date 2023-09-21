7921d02af117fcaec34df6f94b701855b44fe4ae
anaS4ntos <114153519+anaS4ntos@users.noreply.github.com>
2023-09-21 20:24:35 -0300

    Create LoginDTO.cs

---
+++ LoginDTO.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud2023
+{
+    internal class LoginDTO
+    {
+        public string User {get; set;}
+        public string Secret {get; set;}
+        
+        
+
+      
+    }
+}
